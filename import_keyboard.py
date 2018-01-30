3415bc384823b48949e2231f1b404fcb1849b505
jacopods <wilderkde@gmail.com>
2018-01-30 14:43:41 -0500

    Normalize unsculpted SA profile to row 3

--- import_keyboard.py
+++ import_keyboard.py
@@ -281,6 +281,10 @@ def getKey(filePath):
                         else:
                             key["p"] = "DCS"
 
+                        # Normalize unsculpted SA profile to SA ROW 3
+                        if key["p"] = "SA":
+                            key["p"] = "SA3"
+
                         if key["p"] == "" or key["p"] not in ["DSA", "DCS", "SA1","SA2", "SA3", "SA4"]:
                             key["p"] = "DCS"
 
