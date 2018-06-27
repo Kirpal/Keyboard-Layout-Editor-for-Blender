ed6e22319b103841b21a1f4dcaa00441248a148a
Kirpal Demian <kjdemian@gmail.com>
2018-06-27 11:18:02 -0400

    v2.11

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ import bpy
 bl_info = {
     "name": "Import: KLE Raw JSON format (.json)",
     "author": "/u/kdem007 /u/jacopods",
-    "version": (2, 10),
+    "version": (2, 11),
     "blender": (2, 79, 0),
     "location": "File > Import-Export > Keyboard Layout Editor Raw (.json) ",
     "description": "Import Keyboard Layouts",
